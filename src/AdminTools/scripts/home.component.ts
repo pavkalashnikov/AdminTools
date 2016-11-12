c77bdbd99fdc8429598a491d755b0cd3a70ffbed
Kalashnikov Pavel <p.kalashnikov@itransition.com>
2016-11-12 15:42:58 +0300

    Changed start page for Azure

--- src/AdminTools/scripts/home.component.ts
+++ src/AdminTools/scripts/home.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component } from '@angular/core';
 @Component({
     template: `
-        <h1>Home page</h1>
+        <h1>Here is sample application written whith Angular2 to demonstrate Azure Hosting/CI</h1>
     `,
     styles: ['h1 {margin: 0 auto; width: 200px; text-align: center}']
 })
